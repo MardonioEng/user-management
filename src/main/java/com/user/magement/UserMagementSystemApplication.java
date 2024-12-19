5c6d2a0495ceb61fd35fa2ecda71e9a51d6888e9
Mardonio Rodrigues <mardonio.silva@itegra.com.br>
2024-12-19 20:47:43 -0300

    first commit

---
+++ src/main/java/com/user/magement/UserMagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.user.magement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMagementSystemApplication.class, args);
+	}
+
+}
